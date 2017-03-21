6eff1677dcc37f3cd15430675fca7d7506328f8d
云一程 <13718707754@163.com>
2017-03-21 09:54:26 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+print ("hello world")
+#首字母大写，去掉前后空格
+message = " Harry poter "
+print(message.title())
+print(message.lstrip())
+print(message.rstrip())
+print(message.strip())
+#字符串拼接
+first_name = "harry"
+last_name = "poter"
+full_name = first_name + " " + last_name
+message = "hello" + " " + full_name.title() + " " + "!"
+print(message)
+#运算符
+print (3/2)
+print (3 + 4 * 5)
+print((3 + 4) * 5)
